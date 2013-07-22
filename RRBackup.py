61afcb7288946b3871e6c9ab769fb877bc7fbd24
Daniel Meister <dmeister@phys.ethz.ch>
2013-07-22 19:48:53 +0200

    initial release (beta version)

---
+++ RRBackup.py
@@ -0,0 +1,190 @@
+#!/usr/bin/python
+# git repository backup script for the T3
+# written 2013 by Daniel Meister <dmeister@phys.ethz.ch>
+# for details see https://wiki.chipp.ch/twiki/bin/view/CmsTier3/RemoteRepositoryBackup
+
+# imports
+import datetime
+import tempfile
+import os
+import subprocess
+import re
+
+# classes
+class BackupPolicy:
+    '''
+    class defining the backup policy
+    '''
+    def __init__(self,today,days,weeks,months):
+        self.today = today
+        self.DAYS = days
+        self.WEEKS = weeks
+        self.MONTHS = months
+
+    def getkeep(self):
+        keep = set()
+
+        for i in range(self.DAYS):
+            keep.add((self.today - datetime.timedelta(i)).isoformat())
+
+        prev_monday = self.today - datetime.timedelta(self.today.weekday())
+        for i in range(self.WEEKS):
+            keep.add((prev_monday - datetime.timedelta(7 * i)).isoformat())
+            
+        first_monday = prev_monday - datetime.timedelta(self.today.day/7 * 7)
+        for i in range(self.MONTHS):
+            keep.add(first_monday.isoformat())
+            first_monday = first_monday - datetime.timedelta(7)
+            first_monday = first_monday - datetime.timedelta(first_monday.day/7 * 7)
+
+        return keep
+
+class Repository:
+    '''
+    repository definition consisting of the tuple (id, url, active)
+    could be extended to handle different repository types
+    '''
+    def __init__(self,id,url,active):
+        self.id = id
+        self.url = url
+        self.active = active
+
+    def __getitem__(self,attr):
+        return getattr(self,attr)
+
+    def __repr__(self):
+        return 'Repository: %s at %s' % (self.id, self.url)
+
+    def name(self):
+        r = re.compile(".*/([^/]+\.git)")
+        m = r.search(self.url)
+        if m:
+            return m.group(1)
+        raise RepositoryNameException(self)
+        
+
+class RepositoryList:
+    '''
+    iterator that reads list of repositories to backup from config file
+    '''
+    def __init__(self,file):
+        self.lines = [line.strip() for line in open(file)]
+        self.ids = []
+
+    def __iter__(self):
+        self.index = -1
+        return self
+
+    def next(self):
+        # skip lines starting with '#'
+        while True:
+            if self.index >= len(self.lines) - 1:
+                raise StopIteration
+            else:
+                self.index += 1
+                curr = self.lines[self.index]
+                if not curr or curr.startswith('#'):
+                    continue
+                field = curr.split()
+                if not len(field) == 3:
+                    return (None, RepositoryDefinitionException(curr))
+                id = field[0]
+                url = field[1]
+                active = field[2]
+                if id in self.ids:
+                    return (None, RepositoryDuplicateException(id,curr)) 
+                self.ids.append(id)
+                repo = Repository(id,url,active != '0')
+                if not active in ['0','1']:
+                    return (repo, RepositoryActiveException(active))
+                return (repo,None)
+        
+
+class BackupStorage:
+    '''
+    class to handle the storage of the backups
+    for the moment just writing to NFS share but could be easily extended
+    for e.g. storage on dCache
+    '''
+    def __init__(self,base,repo):
+        self.folder = '/'.join([base,repo.id])
+
+    def ensure(self):
+        if not os.path.isdir(self.folder):
+            os.mkdir(self.folder)
+
+    def fullpath(self,date):
+        return self.folder + '/' + date.isoformat() + '.tar.gz'
+
+    def list(self):
+        return set([ f.split('.')[0] for f in os.listdir(self.folder) ])
+
+    def remove(self,list):
+        for item in list:
+            os.remove(self.folder + '/' + item + '.tar.gz')
+
+class RepositoryBackup:
+    '''
+    interface class to bash commands that can actually perform the backup
+    '''
+    def __init__(self,repo):
+        self.repo = repo
+
+    def retrieve(self):
+        self.tmp = tempfile.mkdtemp()
+        ret = subprocess.call([
+            'git','clone',
+            '--mirror','--quiet',
+            self.repo.url,self.tmp + '/' + self.repo.name()
+            ])
+        if not ret == 0:
+            raise RepositoryFetchException(self.repo)
+        
+    def save(self,fullpath):
+        ret = subprocess.call([
+            'tar',
+            '-C',self.tmp,
+            '-czf',fullpath,
+            self.repo.name()
+            ])
+        if not ret == 0:
+            raise RepositoryStoreException(self.repo)
+        try:
+            stat = os.stat(fullpath)
+        except OSError:
+            raise RepositoryStoreException(self.repo)
+        else:
+            if not stat.st_size > 0:
+                raise RepositoryStoreException(self.repo)
+
+    # remove temporary files
+    def clean(self):
+        pass
+
+class RepositoryNameException(Exception):
+    def __init__(self,repo):
+        self.repo = repo
+        self.args = (repo)
+class RepositoryFetchException(Exception):
+    def __init__(self,repo):
+        self.repo = repo
+        self.args = (repo)
+class RepositoryStoreException(Exception):
+    def __init__(self,repo):
+        self.repo = repo
+        self.args = (repo)
+class RepositoryDefinitionException(Exception):
+    def __init__(self,failed):
+        self.failed = failed
+        self.args = (failed)
+class RepositoryDuplicateException(Exception):
+    def __init__(self,id,line):
+        self.id = id
+        self.line = line
+        self.args = (id,line)
+class RepositoryActiveException(Exception):
+    def __init__(self,val):
+        self.val = val
+        self.args = (val)
+
+
---
+++ run_rrb.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# git repository backup script for the T3
+# written 2013 by Daniel Meister <dmeister@phys.ethz.ch>
+# for details see https://wiki.chipp.ch/twiki/bin/view/CmsTier3/RemoteRepositoryBackup
+
+# imports
+from RRBackup import *
+import logging
+
+# constants to define backup policy
+DAYS = 7
+WEEKS = 5
+MONTHS = 12
+# constants to define paths
+CFGFILE = '/swshare/rrbackup/config/backup.list'
+STORAGEPATH = '/swshare/rrbackup/store/'
+LOGFILE =  '/swshare/rrbackup/log/backup.log'
+LOGLEVEL = logging.DEBUG
+
+today = datetime.date.today()
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=LOGLEVEL,
+    filename=LOGFILE
+    )
+
+try:
+
+    bp = BackupPolicy(today,DAYS,WEEKS,MONTHS)
+    keep = bp.getkeep()
+    logging.debug('keeping %d elements: %s' % (len(keep), ','.join(sorted(keep))))
+
+    # loop over all repositories listed in CFGFILE
+    list = RepositoryList(CFGFILE)
+    for repo, exception in list:
+        try:
+            if exception: raise exception
+        except RepositoryDefinitionException, e:
+            # unparsable definition cannot continue with this
+            logging.warning(
+                'the following line of %s could not be processed: %s' %
+                (CFGFILE,' '.join(e.failed.split())))
+            continue
+        except RepositoryDuplicateException, e:
+            # unparsable definition cannot continue with this
+            logging.error(
+                'repository id "%s" already taken; skipping line: %s' %
+                (e.id, ' '.join(e.line.split())))
+            continue
+        except RepositoryActiveException, e:
+            # just assume active to be on the safe side
+            logging.warning(
+                '%(id)s: unknown active value "%(val)s"; assuming IS active' %
+                {'id': repo.id, 'val': e.val})
+
+        logging.info('%(id)s: starting backup of repository at %(url)s' % (repo)) 
+        store = BackupStorage(STORAGEPATH,repo)
+        store.ensure()
+        logging.info(
+            '%(id)s: repository is%(active)s active' %
+            {'id': repo.id, 'active':{True:'',False:' NOT'}[repo.active]})
+        if repo.active:
+            # why do we have such an old version of Python...
+            try:
+                try:
+                    backup = RepositoryBackup(repo)
+                    logging.debug('%(id)s: download remote content' % repo)
+                    backup.retrieve()
+                    logging.debug('%(id)s: store downloaded repository' % repo)
+                    backup.save(store.fullpath(today))
+                except RepositoryNameException, e:
+                    logging.error('%(id)s: could not extract name of repository from URL %(url)s' % e.repo)
+                    continue
+                except RepositoryFetchException, e:
+                    logging.error('%(id)s: could not fetch repository from URL %(url)s' % e.repo)
+                    continue
+            finally:
+                backup.clean()
+
+        have = store.list()
+        logging.debug(
+            '%(id)s: have %(len)d elements: %(list)s' %
+            {'id': repo.id, 'len': len(have), 'list':','.join(sorted(have))})
+        delete = have - keep
+        logging.info(
+            '%(id)s: deleting %(len)d elements: %(list)s' %
+            {'id': repo.id, 'len': len(delete), 'list':','.join(sorted(delete))})
+        store.remove(delete)
+        
+except Exception, e:
+    logging.critical('unhandeled exception (%s): %s' % (e.__class__.__name__, e))
+
+
+''' without caring about exceptions...
+bp = BackupPolicy(today,DAYS,WEEKS,MONTHS)
+keep = bp.getkeep()
+list = RepositoryList(CFGFILE)
+for repo in list:
+    store = BackupStorage(STORAGEPATH,repo)
+    store.ensure()
+    if repo.active:
+        backup = RepositoryBackup(repo)
+        backup.retrieve()
+        backup.save(store.fullpath(today))
+    have = store.list()
+    delete = have - keep
+    store.remove(delete)
+'''
+
+
